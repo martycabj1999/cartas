aadb5caa044f3a373ed39aea2d2b07b898d6c97e
Martin <martingira1999@gmail.com>
2020-05-05 19:54:53 -0300

    graphql configurado

--- src/modules/game/card/components/Card.js
+++ src/modules/game/card/components/Card.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react'
-import '../styles/Card.css'
+import React, { useState } from 'react';
+import '../styles/Card.css';
+import GameProvider from '../../provider/GameProvider';
 
 const Card = () => {
+
+    const [card, setCard] = useState([])
+
+    GameProvider.nextTurn().then(response => {
+        setCard(response.data.card);
+    })
+
     return ( 
         <div className="card">
-            
+            {card}
         </div>
     );
 }
